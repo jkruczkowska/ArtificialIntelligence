5aa82e22c082f51c512b380cd959a4a442f4c533
joannaCL <joanna.kruczkowska@coderslab.pl>
2019-02-07 15:50:12 +0100

    Add face detection

--- nai_project.py
+++ nai_project.py
@@ -6,6 +6,34 @@ import cv2
 import imutils
 import time
 
+# xml files describing our haar cascade classifiers
+faceCascadeFilePath = "haarcascade_frontalface_default.xml"
+noseCascadeFilePath = "haarcascade_nose.xml"
+smileCascadeFilePath = "haarcascade_smile.xml"
+
+# build our cv2 Cascade Classifiers
+faceCascade = cv2.CascadeClassifier(faceCascadeFilePath)
+noseCascade = cv2.CascadeClassifier(noseCascadeFilePath)
+smileCascade = cv2.CascadeClassifier(smileCascadeFilePath)
+
+imgMustache = cv2.imread('mustache.png', -1)
+imgSmiley = cv2.imread('smiley.png', -1)
+
+# Create the mask for the mustache
+orig_mask = imgMustache[:, :, 3]
+orig_mask1 = imgSmiley[:, :, 2]
+
+# Create the inverted mask for the mustache
+orig_mask_inv = cv2.bitwise_not(orig_mask)
+orig_mask_inv1 = cv2.bitwise_not(orig_mask1)
+
+# Convert mustache image to BGR
+# and save the original image size (used later when re-sizing the image)
+imgMustache = imgMustache[:, :, 0:3]
+imgSmiley = imgSmiley[:, :, 0:3]
+origMustacheHeight, origMustacheWidth = imgMustache.shape[:2]
+origSmileyHeight, origSmileyWidth = imgSmiley.shape[:2]
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
                 help="path to the (optional) video file")
@@ -32,13 +60,94 @@ while True:
     if frame is None:
         break
 
-    frame = imutils.resize(frame, width=600)
+    frame = imutils.resize(frame, width=800)
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
     mask = cv2.inRange(hsv, orangeLower, orangeUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
+
+    # Create greyscale image from the video feed
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in input video stream
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+
+    # x_offset = y_offset = 5
+    # smile = imgSmiley
+    # res = cv2.resize(imgSmiley, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    # frame[y_offset:y_offset + imgMustache.shape[0], x_offset:x_offset + imgMustache.shape[1]] = imgMustache
+    # smi = imgSmiley[280:340, 330:390]
+
+    # Iterate over each face found
+    for (x, y, w, h) in faces:
+        # Un-comment the next line for debug (draw box around all faces)
+        face = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 2)
+
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+    # Detect a nose within the region bounded by each face (the ROI)
+        nose = noseCascade.detectMultiScale(roi_gray)
+
+        for (nx, ny, nw, nh) in nose:
+            # Un-comment the next line for debug (draw box around the nose)
+            cv2.rectangle(roi_color, (nx, ny), (nx + nw, ny + nh), (255, 255, 255), 2)
+
+            # The mustache should be three times the width of the nose
+            mustacheWidth = 2 * nw
+            mustacheHeight = mustacheWidth * origMustacheHeight / origMustacheWidth
+
+            # Center the mustache on the bottom of the nose
+            x1 = nx - (mustacheWidth / 4)
+            x2 = nx + nw + (mustacheWidth / 4)
+            y1 = ny + nh - (mustacheHeight / 2)
+            y2 = ny + nh + (mustacheHeight / 2)
+
+            # Check for clipping
+            if x1 < 0:
+                x1 = 0
+            if y1 < 0:
+                y1 = 0
+            if x2 > w:
+                x2 = w
+            if y2 > h:
+                y2 = h
+
+            # Re-calculate the width and height of the mustache image
+            mustacheWidth = x2 - x1
+            mustacheHeight = y2 - y1
+
+            # Re-size the original image and the masks to the mustache sizes
+            # calcualted above
+            mustache = cv2.resize(imgMustache, (mustacheWidth, mustacheHeight), interpolation=cv2.INTER_AREA)
+            mask = cv2.resize(orig_mask, (mustacheWidth, mustacheHeight), interpolation=cv2.INTER_AREA)
+            mask_inv = cv2.resize(orig_mask_inv, (mustacheWidth, mustacheHeight), interpolation=cv2.INTER_AREA)
+
+            # take ROI for mustache from background equal to size of mustache image
+            roi = roi_color[y1:y2, x1:x2]
+
+            # roi_bg contains the original image only where the mustache is not
+            # in the region that is the size of the mustache.
+            roi_bg = cv2.bitwise_and(roi, roi, mask=mask_inv)
+
+            # roi_fg contains the image of the mustache only where the mustache is
+            roi_fg = cv2.bitwise_and(mustache, mustache, mask=mask)
+
+            # join the roi_bg and roi_fg
+            dst = cv2.add(roi_bg, roi_fg)
+
+            # place the joined image, saved to dst back over the original image
+            roi_color[y1:y2, x1:x2] = dst
+
+            break
 
     # (x, y) center of the ball
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
